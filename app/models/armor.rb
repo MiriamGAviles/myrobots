b0d68c99baa4ff26d25004fdb0a1c03f3faf2681
Miriam <miriam.garcia.aviles@gmail>
2019-11-29 12:13:47 +0100

    change db

--- app/models/armor.rb
+++ app/models/armor.rb
@@ -1,3 +1,6 @@
 class Armor < ApplicationRecord
-	belongs_to :robots, required :true
+	belongs_to :robots, required: true
+
+	validates :a_type, presence: true, inclusion: { in: ["shield", "magnetic field", "invisible field"]}
+  	validates :serial_number, format: { with: /\AA+\d{6}\z/}
 end
--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,3 +1,6 @@
 class Weapon < ApplicationRecord
-	belongs_to :robots, required :true
+	belongs_to :robots, required: true
+
+	validates :weapon_type, presence: true, inclusion: { in: %w(laser code acid map)}
+  	validates :serial_number, format: { with: /\AW+\d{6}\z/}
 end
